82a7c48f136970eccc4dc8753a5f21570bc02ef1
Andrea Duina <andreaduina22@gmail.com>
2025-10-06 00:14:00 +0200

    [qpu] added qpu module and cw qubit

--- sqil_experiments/measurements/setup/cw.py
+++ sqil_experiments/measurements/setup/cw.py
@@ -8,15 +8,14 @@ from sqil_experiments.measurements.helpers.sqil_transmon.operations import (
     SqilTransmonOperations,
 )
 from sqil_experiments.measurements.helpers.sqil_transmon.qubit import SqilTransmon
+from sqil_experiments.qpu.cw.operations import CwQubitOperations
+from sqil_experiments.qpu.cw.qubit import CwQubit
 
-n_qubit = 1
+n_qubits = 1
 
 # Checklist for every cooldown
 # - update data_folder_name
-# - update the initial_readout_lo_freq, an approximate value is required to run onetone
-data_folder_name = "20251001_stormcrow_N17"
-initial_readout_lo_freq = 7.2e9
-
+data_folder_name = "20251005_stormcrow_N17"
 
 # Data storage
 db_root = r"Z:\Projects\BottomLoader\data"
@@ -29,26 +28,10 @@ storage = {
 }
 
 
-# Zurich Instruments setup
-def generate_zi_setup():
-    qt_platform = demo_platform(n_qubits=n_qubit)
-    return qt_platform.setup
-
-
-# zi_setup = DeviceSetup.from_descriptor(zi_descriptor, "localhost")
-def generate_qpu(zi_setup):
-    qubits = SqilTransmon.from_device_setup(zi_setup)
-    quantum_operations = SqilTransmonOperations()
+def generate_qpu(*args):
+    qubits = [CwQubit(f"q{i}") for i in range(n_qubits)]
+    quantum_operations = CwQubitOperations()
     qpu = QPU(qubits, quantum_operations)
-
-    # Set required qubit parameters
-    for qubit in qpu.quantum_elements:
-        qubit.update(
-            **{
-                "readout_lo_frequency": initial_readout_lo_freq,
-                "drive_lo_frequency": 5e9,
-            }
-        )
     return qpu
 
 
@@ -60,25 +43,17 @@ instruments = {
         "name": "SGSA100",
         "address": "TCPIP0::192.168.1.201::inst0::INSTR",
         "variables": {
-            "frequency": lambda exp: (
-                exp.qpu.quantum_elements[0].parameters.readout_external_lo_frequency
-            ),
-            "power": lambda exp: (
-                exp.qpu.quantum_elements[0].parameters.readout_external_lo_power
-            ),
+            "frequency": bind_instrument_qubit("drive_frequency", "q0"),
+            "power": bind_instrument_qubit("drive_power", "q0"),
         },
-    },
-    "zi": {
-        "type": "ZI",
-        "address": "localhost",
-        "generate_setup": generate_zi_setup,
-        "generate_qpu": generate_qpu,
     },
     "yoko0": {
         "type": "CS",
         "model": "Yokogawa_GS200",
         "address": "TCPIP0::192.168.1.199::inst0::INSTR",
         "name": "yoko0",
+        "ramp_step": 1e-6,
+        "ramp_step_delay": 0.008,
         "variables": {"current": bind_instrument_qubit("current", "q0")},
     },
     "vna": {
@@ -87,6 +62,6 @@ instruments = {
         "name": "vna",
         "address": "TCPIP0::192.168.1.203::inst0::INSTR",
         "s_param": "S21",
-        "variables": {"power": bind_instrument_qubit("cw_ro_power", "q0")},
+        "variables": {"power": bind_instrument_qubit("readout_power", "q0")},
     },
 }
